d3eeba8198fe06f13175f690c36c68f82787561a
juldhyne <julien.dhyne@gmail.com>
2025-06-26 22:11:41 +0200

    character update is now abstract

--- src/models/character-update.ts
+++ src/models/character-update.ts
@@ -1,7 +1,50 @@
-export class CharacterUpdate {
+import { Character } from "./character";
+
+export abstract class CharacterUpdate {
   constructor(
     public readonly sourceId: string,
     public readonly targetId: string,
-    public readonly damages: number,
   ) {}
+
+  abstract applyToCharacter(c: Character): Character;
+
+  static damage(
+    sourceId: string,
+    targetId: string,
+    amount: number,
+  ): DamageUpdate {
+    return new DamageUpdate(sourceId, targetId, amount);
+  }
+
+  static heal(sourceId: string, targetId: string, amount: number): HealUpdate {
+    return new HealUpdate(sourceId, targetId, amount);
+  }
+}
+
+export class DamageUpdate extends CharacterUpdate {
+  constructor(
+    sourceId: string,
+    targetId: string,
+    public readonly amount: number,
+  ) {
+    super(sourceId, targetId);
+  }
+
+  applyToCharacter(c: Character): Character {
+    return c.applyDamage(this.amount);
+  }
+}
+
+export class HealUpdate extends CharacterUpdate {
+  constructor(
+    sourceId: string,
+    targetId: string,
+    public readonly amount: number,
+  ) {
+    super(sourceId, targetId);
+  }
+
+  applyToCharacter(c: Character): Character {
+    return c.applyHeal(this.amount);
+  }
 }
