c19561ebdf74a095c5f22e129ab7535aad07b721
juldhyne <julien.dhyne@gmail.com>
2025-06-26 22:11:59 +0200

    add methods on character

--- src/models/character.ts
+++ src/models/character.ts
@@ -1,8 +1,20 @@
 export class Character {
-  constructor(public readonly id: string, public readonly hp: number) {}
+  constructor(
+    public readonly id: string,
+    public readonly hp: number,
+    public readonly maxHp: number,
+  ) {}
 
   applyDamage(damage: number): Character {
-    return new Character(this.id, Math.max(this.hp - damage, 0));
+    return new Character(this.id, Math.max(this.hp - damage, 0), this.maxHp);
+  }
+
+  applyHeal(heal: number): Character {
+    return new Character(
+      this.id,
+      this.hp === 0 ? 0 : Math.min(this.hp + heal, this.maxHp),
+      this.maxHp,
+    );
   }
 
   isAlive(): boolean {
