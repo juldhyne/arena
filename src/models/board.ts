40eb96a2700d6607d9ff934c66b72241d8b37e57
juldhyne <julien.dhyne@gmail.com>
2025-06-27 16:08:34 +0200

    add board to game state

---
+++ src/models/board.ts
@@ -0,0 +1,8 @@
+export class Board {
+  constructor(
+    public readonly minX: number,
+    public readonly maxX: number,
+    public readonly minY: number,
+    public readonly maxY: number,
+  ) {}
+}
--- src/models/game-state.ts
+++ src/models/game-state.ts
@@ -1,14 +1,19 @@
+import { Board } from "./board";
 import { Character } from "./character";
 import { CharacterUpdate } from "./character-update";
 
 export class GameState {
-  constructor(public readonly characters: Character[]) {}
+  constructor(
+    public readonly characters: Character[],
+    public readonly board: Board,
+  ) {}
 
   applyUpdate(update: CharacterUpdate): GameState {
     return new GameState(
       this.characters.map((c) =>
         c.id === update.targetId ? update.applyToCharacter(c, this) : c,
       ),
+      this.board,
     );
   }
 }
